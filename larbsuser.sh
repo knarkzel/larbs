cba16de998927b836e22317b7aa2d41fc23c71ee
Odd-Harald Lillestø Myhren <ohmyhren@uia.no>
2021-08-21 00:59:30 +0200

    force push in user script

--- larbsuser.sh
+++ larbsuser.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd ~
 rm .bashrc
+rm .bash_profile
 mkdir downloads
 mkdir -p source/rust
 git clone --bare https://gitlab.com/knarkzel/dotfiles $HOME/.dotfiles
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
 startx
